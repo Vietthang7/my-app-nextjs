328bde61dde9da93a7f32b8c8a44cfba59ad5248
thangnguyen <nguyenvietthang12092004@gmail.com>
2025-08-07 17:47:54 +0700

    first

---
+++ app/components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="bg-[#F5F5F5] container">
+        <div className="px-30">
+            <div className="h-[14px] w-[214px]">Welcome to worldwide Megamart!</div>
+        </div>
+      </div>
+    </header>
+  );
+}
